a3a0108a3d84a0389777ec95ce5530e758457e50
Anupriya Goyal <anupriyagoyal14@gmail.com>
2020-04-09 10:30:42 -0400

    Add files via upload

---
+++ Backspace String Compare.java
@@ -0,0 +1,54 @@
+//Backspace String Compare
+
+/*Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+*/
+
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        
+        Stack<Character> sStack= new Stack<Character>();
+        
+        for(char c:S.toCharArray())
+        {
+            if(c!='#')
+            {
+                sStack.push(c);
+            }
+            else if(!sStack.isEmpty())
+            {
+                sStack.pop();
+            }
+        }
+        
+        Stack<Character> tStack= new Stack<Character>();
+        
+        for(char c:T.toCharArray())
+        {
+            if(c!='#')
+            {
+                tStack.push(c);
+            }
+            else if(!tStack.isEmpty())
+            {
+                tStack.pop();
+            }
+        }
+        
+        while(!sStack.isEmpty())
+        {
+            char current= sStack.pop();
+            
+            if(tStack.isEmpty() || tStack.pop()!= current)
+            {
+                return false;
+            }
+        }
+        
+        return sStack.isEmpty() && tStack.isEmpty();
+    }
+}
